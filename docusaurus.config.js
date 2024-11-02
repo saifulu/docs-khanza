b2807f5fc9eed6cb74b25cb72c15fecd881a8faf
saiful umam <saifulumam@MacBook-Air-saiful.local>
2024-11-03 06:28:41 +0700

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,113 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const config = {
+  title: 'Dokumentasi SIMKES Khanza',
+  tagline: 'Solusi Cerdas Untuk Manajemen Kesehatan',
+  url: 'https://docs.faskesku.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/mas-elkhanza/SIMRS-Khanza',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'hhttps://github.com/mas-elkhanza/SIMRS-Khanza',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'YASKI',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/yaski-logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Docs',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/mas-elkhanza/SIMRS-Khanza',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/mas-elkhanza/SIMRS-Khanza',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,64 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Mudah Digunakan',
+    Svg: require('@site/static/img/undraw_medcine.svg').default,
+    description: (
+      <>
+        SIMKES Khanza dirancang untuk mempermudah pengelolaan informasi kesehatan. 
+        Instalasi dan penggunaan yang mudah memungkinkan sistem ini segera berjalan dengan lancar.
+      </>
+    ),
+  },
+  {
+    title: 'Keamanan Data Pasien',
+    Svg: require('@site/static/img/undraw_safe_re_kiil.svg').default,
+    description: (
+      <>
+        Dengan menyimpan data pasien dalam <code>DATABASE</code> lokal Fasilitas Kesehatan, 
+        SIMKES Khanza meminimalkan risiko pelanggaran data.
+      </>
+    ),
+  },
+  {
+    title: 'OpenSource Software',
+    Svg: require('@site/static/img/undraw_pair_programming_re_or4x.svg').default,
+    description: (
+      <>
+        Dengan menggunakan teknologi terbuka *(Open Source)*, kita bersama-sama membangun 
+        SIMKES Khanza untuk masa depan kesehatan yang lebih baik. Setiap kontribusi sangat berarti!
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
